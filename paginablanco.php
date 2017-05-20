0001a040437932bf49e39d415190a5226dbcadce
JoseHumbertoSanchezGaleana <beto_9303@hotmail.com>
2017-05-20 13:04:30 -0500

    Add files via upload

---
+++ paginablanco.php
@@ -0,0 +1,123 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>GR Ingenier&iacute;a y Construcci&oacute;n</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+         <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/main.css" />
+        <link rel="shortcut icon" type="image/x-icon" href="images/icono.png">
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+								<header id="header">
+									<a href="index.html" class="logo"><strong>INGENIERIA</strong> y CONSTRUCCION</a>
+									<ul class="icons">
+										<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+										<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+										<li><a href="#" class="icon fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>
+										<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="#" class="icon fa-medium"><span class="label">Medium</span></a></li>
+									</ul>
+								</header>
+							       <section>
+                                       <center>
+                                       	<h3>Aqui va los datos del trabajador y su rol</h3>
+
+                                       </center>
+                            </section>
+
+						</div>
+					</div>
+
+				<!-- Sidebar -->
+					<div id="sidebar">
+						<div class="inner">
+
+							<!-- Search -->
+								<section id="search" class="alt">
+									<form method="post" action="#">
+										<input type="text" name="query" id="query" placeholder="Search" />
+									</form>
+								</section>
+
+							<!-- Menu -->
+								<nav id="menu">
+									<header class="major">
+										<h2>Menu</h2>
+									</header>
+									<ul>
+										<li><a href="index.html">INICIO</a></li>
+										<li><a href="noticias.html">NOTICIAS</a></li>
+										<li><a href="somos.html">QUIENES SOMOS</a></li>
+										<li>
+											<span class="opener">REGISTROS</span>
+											<ul>
+												<li><a href="regcliente.php">Registrar Cliente</a></li>
+												<li><a href="regmaquinaria.php">Registrar Maquinaria </a></li>
+												<li><a href="regtrabajador.php">REgistrar Trabajador</a></li>
+												
+											</ul>
+										</li>
+                                        <li><a href="consultar.php">CONSULTAS</a></li>
+										
+								
+								    </ul>
+								</nav>
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>Proyecto Actual</h2>
+									</header>
+									<div class="mini-posts">
+										<article>
+											<a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
+											<p>Este es el proyecto actual...</p>
+										</article>
+									</div>
+									<ul class="actions">
+										<li><a href="#" class="button">More</a></li>
+									</ul>
+								</section>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>Contactanos</h2>
+									</header>
+									<ul class="contact">
+										<li class="fa-envelope-o"><a href="#">information@untitled.tld</a></li>
+										<li class="fa-phone">(000) 000-0000</li>
+										<li class="fa-home">1234 Somewhere Road #8254<br />
+										Nashville, TN 00000-0000</li>
+									</ul>
+								</section>
+
+							<!-- Footer -->
+								<footer id="footer">
+									<p class="copyright">&copy; Untitled. All rights reserved. Ingenieriá y Construccion.</p>
+								</footer>
+
+						</div>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <script type="text/javascript"></script>>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
